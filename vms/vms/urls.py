e9697784fde7e09796ccde4701c13dea24b79d2a
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2020-05-17 21:51:52 +0530

    Revert "Code: Add staticfiles to urlpatterns in vms/urls.py"
    This reverts commit 22f2de5902e20b9e06c7e9a801f226204399a02f.

--- vms/vms/urls.py
+++ vms/vms/urls.py
@@ -1,9 +1,6 @@
 # Django
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls import include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -25,6 +22,3 @@ urlpatterns = i18n_patterns(
     url(r'^volunteer/', include('volunteer.urls', namespace='volunteer')),
 )
 
-if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
